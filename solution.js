a0ac2fa70f3ca1bd35c2e477d2d5736c129c176f
LalaChristl <lesleyannchristl@gmail.com>
2022-06-29 14:32:39 +0200

    added work to solutions.js

--- solution.js
+++ solution.js
@@ -1,1 +1,27 @@
+let str = 'Lesley-Ann';
+console.log('str');
+
+console.log('second element is', str[1])
+console.log('sixth element is', str[5])
+
+const firstName = 'Lala';
+let age = '38';
+let year = '2022';
+
+console.log(firstName + " is " + age + " in " + year)
+
+let isMarried = 'True';
+
+console.log(firstName + " is married: " + isMarried)
+
+const programmingLanguage = 'Java Script';
+let isFun = 'True';
+
+console.log(programmingLanguage + " is fun " + isFun)
+
+console.log('first element is', programmingLanguage[0])
+console.log('fifth element is', programmingLanguage[5])
+console.log('length of string is', programmingLanguage.length)
+
+
 
